cfb6ffac81b4249f22e5d928b983018e74769402
Carlo Sciolla <carlo.sciolla@gmail.com>
2012-01-18 22:36:22 +0100

    End of meetup #26

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "Amsterdam Clojurians website"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
-                 [ring "1.0.1"]]
+                 [ring "1.0.1"]
+                 [enlive "1.0.0"]
+                 [org.clojure/data.json "0.1.1"]]
   :resources-path "resources")
--- src/ams26/core.clj
+++ src/ams26/core.clj
@@ -3,10 +3,34 @@
         compojure.core
         compojure.route
         ring.middleware.file
-        ring.util.response))
+        ring.util.response
+        net.cgrand.enlive-html
+        clojure.data.json))
+
+(def api-key (System/getenv "meetup_key"))
+
+(defn meetup-event-url [key]
+  (str "https://api.meetup.com/2/events?key="
+       key
+       "&sign=true&status=upcoming&group_urlname=The-Amsterdam-Clojure-Meetup-Group&page=1"))
+
+(defn fetch-event [key]
+  (first
+   (:results
+    (read-json
+     (slurp (meetup-event-url key))))))
+
+(deftemplate home "public/index.html" [api-key]
+  [:title] (content "Amsterdam Clojurians")
+  [:a.brand] (content "Amsterdam Clojurians Meetup")
+  [:#next-meetup] (content ()))
+
+(defn home-wrap [req]
+  {:pre [(:key req)]}
+  (home (:key req)))
 
 (defroutes application
-  (GET "/" [] (redirect "/index.html"))
+  (GET "/" [] home)
   (resources "/"))
 
 (defn start-server
